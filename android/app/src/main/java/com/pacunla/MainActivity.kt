80e641a1260f65d0e73ea29a4595aa59cdef0593
Bea <lynziepacunla@gmail.com>
2026-02-16 18:58:26 +0800

    Initial commit

--- android/app/src/main/java/com/pacunla/MainActivity.kt
+++ android/app/src/main/java/com/pacunla/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.pacunla
 
+
+import android.os.Bundle
+import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+
 
 class MainActivity : ReactActivity() {
 
@@ -13,6 +17,10 @@ class MainActivity : ReactActivity() {
    */
   override fun getMainComponentName(): String = "PACUNLA"
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
+    super.onCreate(savedInstanceState)
+  }
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]

35c4b4087dfed4871a82cd7382e9afc47efb1ace
Bea <lynziepacunla@gmail.com>
2026-05-04 15:37:53 +0800

    refactor: remove AsyncStorage persistence

--- App.js
+++ App.js
@@ -3,11 +3,21 @@ import { View } from 'react-native';
 
 import AppNavigationNi from './src/navigations';
 
+import rootSaga from './src/app/sagas';
+import configureStore from './src/app/reducers';
+
+import { Provider } from 'react-redux';
+
+const { store, runSaga } = configureStore();
+runSaga(rootSaga);
+
 const App = () => {
   return (
-    <View style={{ flex: 1 }}>
-      <AppNavigationNi />
-    </View>
+    <Provider store={store}>
+      <View style={{ flex: 1 }}>
+        <AppNavigationNi />
+      </View>
+    </Provider>
   );
 };
 
